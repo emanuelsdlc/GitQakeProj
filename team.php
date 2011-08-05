ddfb7a0ee184d3aafc2b843506deefc108721472
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-05 02:00:34 -0700

    Initialize again -_-

---
+++ team.php
@@ -0,0 +1,85 @@
+<!doctype html>
+
+<html>
+
+    <head>
+        <title>Qake - Team</title>
+        <meta charset="UTF-8" >
+        <link rel="stylesheet" href="style.css" type="text/css" media="all" />
+    </head>
+    
+    <body>
+    	<div id="container">
+    		<header id="header">
+    			Qake Logo
+    		</header>
+    		<div id="main">
+    			<section>
+    				<header>
+    					<h1>Qake - Team</h1>
+    				</header>
+    				<section class="team_display">
+    					<div class="team_member_picture">
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/275996_100002270444913_3349129_n.jpg" title="Quynh Nguyen - Team Lead" alt="Quynh Nguyen" />
+    					</div>
+    					<div class="team_member_info">
+    						<p><b>Name: </b> Quynh Nguyen</p>
+    						<p><b>Role: </b> Team Lead</p>
+    						<p><b>Contact: </b>qun003@ucsd.edu</p>
+    						<p><b>Bio: </b> Sleep, School, Homework, Programing, and Gaming</p>
+    					</div>
+    				</section><br class="clear" />
+    				
+    				<section class="team_display">
+    					<div class="team_member_picture">
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/187132_1445860459_4773110_n.jpg" title="" alt="Emanuel Sanchez-de la Cerda" />
+    					</div>
+    					<div class="team_member_info">
+    						<p><b>Name: </b>Emanuel Sanchez-de la Cerda</p>
+    						<p><b>Role: </b> Enter your position here </p>
+    						<p><b>Contact: </b>emanuel.sdlc@gmail.com</p>
+    						<p><b>Bio: </b> None</p>
+    					</div>
+    				</section><br class="clear" />
+    				
+    				<section class="team_display">
+    					<div class="team_member_picture">
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/23242_1510892550_8693_n.jpg" itle="" alt="Aria Arasteh" />
+    					</div>
+    					<div class="team_member_info">
+    						<p><b>Name: </b> Aria Arasteh</p>
+    						<p><b>Role: </b> Enter your position here </p>
+    						<p><b>Contact: </b>ariaarasteh@gmail.com</p>
+    						<p><b>Bio: </b> None</p>
+    					</div>
+    				</section><br class="clear" />
+    				
+    				<section class="team_display">
+    					<div class="team_member_picture">
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/174483_619173801_5567772_n.jpg" itle="" alt="Kelvin Lapidario" />
+    					</div>
+    					<div class="team_member_info">
+    						<p><b>Name: </b> Kelvin Lapidario</p>
+    						<p><b>Role: </b> Enter your position here </p>
+    						<p><b>Contact: </b>klapidar@ucsd.edu</p>
+    						<p><b>Bio: </b> Kelvin..find a better mugshot picture of yourself and update the default one lol</p>
+    					</div>
+    				</section><br class="clear" />
+    				
+    				
+    				
+    				<footer>
+    					For more information about our team please contact us at admin@qake.info
+    				</footer>
+    			</section>
+    		</div>
+    		<footer id="footer">
+    			&copy; Qake.info
+    		</footer>
+    	</div>
+    	
+    	
+    </body>
+
+</html>
+
