84d2e1ac0695ffcdb98ac5a0fda25bb7aaa073db
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-05 02:23:40 -0700

    fix html errors for validation

--- team.php
+++ team.php
@@ -7,7 +7,7 @@
         <meta charset="UTF-8" >
         <link rel="stylesheet" href="style.css" type="text/css" media="all" />
     </head>
-    
+
     <body>
     	<div id="container">
     		<header id="header">
@@ -29,7 +29,7 @@
     						<p><b>Bio: </b> Sleep, School, Homework, Programing, and Gaming</p>
     					</div>
     				</section><br class="clear" />
-    				
+
     				<section class="team_display">
     					<div class="team_member_picture">
     						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/187132_1445860459_4773110_n.jpg" title="" alt="Emanuel Sanchez-de la Cerda" />
@@ -41,10 +41,10 @@
     						<p><b>Bio: </b> None</p>
     					</div>
     				</section><br class="clear" />
-    				
+
     				<section class="team_display">
     					<div class="team_member_picture">
-    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/23242_1510892550_8693_n.jpg" itle="" alt="Aria Arasteh" />
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-ash2/23242_1510892550_8693_n.jpg" title="" alt="Aria Arasteh" />
     					</div>
     					<div class="team_member_info">
     						<p><b>Name: </b> Aria Arasteh</p>
@@ -53,10 +53,10 @@
     						<p><b>Bio: </b> None</p>
     					</div>
     				</section><br class="clear" />
-    				
+
     				<section class="team_display">
     					<div class="team_member_picture">
-    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/174483_619173801_5567772_n.jpg" itle="" alt="Kelvin Lapidario" />
+    						<img src="http://profile.ak.fbcdn.net/hprofile-ak-snc4/174483_619173801_5567772_n.jpg" title="" alt="Kelvin Lapidario" />
     					</div>
     					<div class="team_member_info">
     						<p><b>Name: </b> Kelvin Lapidario</p>
@@ -65,9 +65,9 @@
     						<p><b>Bio: </b> Kelvin..find a better mugshot picture of yourself and update the default one lol</p>
     					</div>
     				</section><br class="clear" />
-    				
-    				
-    				
+
+
+
     				<footer>
     					For more information about our team please contact us at admin@qake.info
     				</footer>
@@ -77,8 +77,8 @@
     			&copy; Qake.info
     		</footer>
     	</div>
-    	
-    	
+
+
     </body>
 
 </html>
