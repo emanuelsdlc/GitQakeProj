2cd17987c7c6b6d784d0afc4130b8f39cdfc903d
Kelvin Lapidario <kelvin.lapidario@gmail.com>
2011-08-06 16:05:42 -0700

    Error pages and favicon

---
+++ 403.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" >
+		<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+		<title>Qake - 403 ERROR!</title>
+	</head>
+<body>
+    	<div id="container">
+    		<header id="header">
+    			Qake Logo
+    		</header>
+    		<div id="main">
+    			<section>
+    				<header>
+    					<h1>Qake - Team</h1>
+    				</header>
+    				<h1>ERROR: 403 Forbidden Page.</h1>
+				<p>Sorry but you are trying to access a page, file or directory that should not be accessed.</p>
+    				<img src="cakeError.jpg" alt="The Qake is a lie" height=30% width=30% />
+				<p>You can't have your Qake and eat it too...</p> 
+				<p>Click <a href="./index.html">here</a> to return from Qake coma.</p>
+				<footer>
+    					For more information about our team please contact us at admin@qake.info
+    				</footer>
+    			</section>
+    		</div>
+    		<footer id="footer">
+    			&copy; Qake.info
+    		</footer>
+    	</div>
+    </body>
+</html>
+
