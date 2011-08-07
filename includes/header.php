1210d3a29df98df3ed69c64da6d0729e391e0280
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-07 11:54:39 -0700

    removed bad meta tag

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,6 @@
         <title>Qake - <?php if($_GET['__title']) echo $_GET['__title']; ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" >
         <meta name="author" content="Qake" />
-		<meta name="Distribution" content="Global" />
         <link rel="stylesheet" href="./includes/production/css/style-min.css" type="text/css" media="all" />
         <script type="text/javascript" src="./includes/production/js/global-min.js"></script>
 		
