c403699ac15e1e41460f81df7c95b8c4c6b15735
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-07 11:38:23 -0700

    compressed js/css for production

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,10 @@
     <head>
         <title>Qake - <?php if($_GET['__title']) echo $_GET['__title']; ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" >
-        <link rel="stylesheet" href="./includes/style.css" type="text/css" media="all" />
-        <script type="text/javascript" src="./includes/global.js"></script>
+        <meta name="author" content="Qake" />
+		<meta name="Distribution" content="Global" />
+        <link rel="stylesheet" href="./includes/production/css/style-min.css" type="text/css" media="all" />
+        <script type="text/javascript" src="./includes/production/js/global-min.js"></script>
 		
     </head>
 
