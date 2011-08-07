cdeacc2a86703d2a6c270e7753bda19103f2d13a
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-07 12:17:03 -0700

    restore Kelvin's favicon

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,8 @@
         <title>Qake - <?php if($_GET['__title']) echo $_GET['__title']; ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" >
         <meta name="author" content="Qake" />
+        <link rel="icon" href="/favicon.ico" />
+	<link rel="shortcut icon" href="/favicon.ico" />
         <link rel="stylesheet" href="./includes/production/css/style-min.css" type="text/css" media="all" />
         <script type="text/javascript" src="./includes/production/js/global-min.js"></script>
 		
