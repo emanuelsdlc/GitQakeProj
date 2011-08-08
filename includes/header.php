cce9fb3c7b0242ade54f3433a6a01c0dd6f9d3b3
Quynh Nguyen <quynhnguyen003@gmail.com>
2011-08-07 19:53:58 -0700

    full path to css and js

--- includes/header.php
+++ includes/header.php
@@ -8,14 +8,14 @@
         <meta name="author" content="Qake" />
         <link rel="icon" href="/favicon.ico" />
 	<link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="./includes/production/css/style-min.css" type="text/css" media="all" />
-        <script type="text/javascript" src="./includes/production/js/global-min.js"></script>
+        <link rel="stylesheet" href="http://www.qake.info/includes/production/css/style-min.css" type="text/css" media="all" />
+        <script type="text/javascript" src="http://www.qake.info/includes/production/js/global-min.js"></script>
 		
     </head>
 
     <body>
     	<div id="container">
     		<header id="header">
-    			<a href="http://www.qake.info/"><img src="./images/cake-logo.png" alt="Qake Logo" title="Qake.info" border="0"/> </a><span>Welcome to Qake!</span>
+    			<a href="http://www.qake.info/"><img src="http://www.quake.info/images/cake-logo.png" alt="Qake Logo" title="Qake.info" border="0"/> </a><span>Welcome to Qake!</span>
     		</header>
     		<div id="main">
